613ef916ffc3ae6e4191bfcf8400cc9d49a183ab
bradenasmith2 <bradenasmith@icloud.com>
2023-11-06 13:58:35 -0600

    Add project files.

---
+++ bradenasmith/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bradenasmith.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
